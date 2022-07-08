e205bd001d52c63e02cf817b9c0a8cccb0e39417
skkkyyyeee <anoopru2002@gmail.com>
2022-07-08 13:12:27 +0530

    galary app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosGalaryComponent } from './photos-galary/photos-galary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosGalaryComponent
   ],
   imports: [
     BrowserModule,
